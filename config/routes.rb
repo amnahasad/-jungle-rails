24f0685c67a6873b1f837a760e2e77fda26924a2
Amnah Asad <amnah_asad@hotmail.com>
2022-01-11 02:54:22 +0000

    Updated the form layout page

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  
+
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  post '/signup' => 'users#create'
 
   get "about", to: "about#show"
 
