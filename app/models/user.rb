cfb22e824408235e8b4da294ed120baa33ecdd98
Amnah Asad <amnah_asad@hotmail.com>
2022-01-12 19:52:32 +0000

    Defined validation specs for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ActiveRecord::Base
 
     has_secure_password
 
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: {case_sensitive: false}
+    validates :password, presence: true
     validates :password_digest, presence: true
 
+def name
+ first_name
+end
 
-    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  # name:
-  # email:
-  # password:
-  # password_confirmation:
-
 
   # @user = User.create(
   #       name: "Bob", 
@@ -17,31 +11,103 @@ RSpec.describe User, type: :model do
   #       expect(@user).to be_falsey
 
 
-
   describe 'Validations' do
     
     it 'should save with all fields filled out of the form' do
-      
+      @user = User.new(
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bob@bob.com',
+        password: '123', 
+        password_confirmation: '123'
+      )
+      expect(@user).to be_valid
     end
 
     it 'is not valid if the first name is missing' do
-      
+      @user = User.new(
+        first_name: nil,
+        last_name: 'Smith',
+        email: 'bob@bob.com',
+        password: '123', 
+        password_confirmation: '123'
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it 'is not valid if the last name is missing' do
-      
+      @user = User.new(
+        first_name: 'Bob',
+        last_name: nil,
+        email: 'bob@bob.com',
+        password: '123', 
+        password_confirmation: '123'
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it 'is not valid if the email is missing' do
-      
+      @user = User.new(
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: nil,
+        password: '123', 
+        password_confirmation: '123'
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'is not valid if the password is missing' do
-      
+      @user = User.new(
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bob@bob.com',
+        password: nil, 
+        password_confirmation: '123'
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
     it 'is not valid if the password and password confirmation do not match' do
-      
+      @user = User.new(
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bob@bob.com',
+        password: 'apple', 
+        password_confirmation: 'apple'
+      )
+      @user2 = User.new(
+        first_name: 'Ellen',
+        last_name: 'Alex',
+        email: 'ellen@ellen.com',
+        password: 'salad', 
+        password_confirmation: 'SALAD'
+      )
+      expect(@user.password).to eq(@user.password_confirmation)
+      expect(@user2.password).to_not eq(@user2.password_confirmation)
+    end
+
+    it 'is not valid if the email is not unique' do
+      @user = User.create(
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bob@bob.com',
+        password: 'apple', 
+        password_confirmation: 'apple'
+      )
+      @user2 = User.create(
+        first_name: 'Bobby',
+        last_name: 'Alex',
+        email: 'bob@bob.com',
+        password: 'apple', 
+        password_confirmation: 'apple'
+      )
+      expect(@user2).to be_invalid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
     end
 
   end
