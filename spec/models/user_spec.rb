431cb7f8047c0eba5861d375393db4873ec5a609
Amnah Asad <amnah_asad@hotmail.com>
2022-01-12 01:58:15 +0000

    Added initial setup of user examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # name:
+  # email:
+  # password:
+  # password_confirmation:
+
+
+  # @user = User.create(
+  #       name: "Bob", 
+  #       email: 'bob@bob.com',
+  #       password: '123', 
+  #       password_confirmation: '123'
+  #     )
+  #       expect(@user).to be_falsey
+
+
+
+  describe 'Validations' do
+    
+    it 'should save with all fields filled out of the form' do
+      
+    end
+
+    it 'is not valid if the first name is missing' do
+      
+    end
+
+    it 'is not valid if the last name is missing' do
+      
+    end
+
+    it 'is not valid if the email is missing' do
+      
+    end
+
+    it 'is not valid if the password is missing' do
+      
+    end
+
+    it 'is not valid if the password and password confirmation do not match' do
+      
+    end
+
+  end
+
 end
